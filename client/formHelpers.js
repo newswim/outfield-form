fc5bfb1144114be99ed8822b91988327ecebc07c
Dan Minshew <ofenixculpa@gmail.com>
2015-06-07 20:10:03 -0500

    some edit session switching, added Handlebar Helpers and X-editable

--- client/formHelpers.js
+++ client/formHelpers.js
@@ -14,12 +14,18 @@ Template.customerTable.events({
 	"click [data-action='remove']" : function(doc){
 		console.log("Destroyed record for " + this.first + " " + this.last + ", id: " + this._id);
 		// remove from collection by id
+		// var self = this;
 		// var doc = {
-		// 	name: this.first + " " + this.last,
-		// 	company: this.company,
-		// 	type: this.type
+		// 	name: self.first + " " + self.last,
+		// 	company: self.company,
+		// 	type: self.type
 		// }
 		// return Archives.insert(doc);
     return Customers.remove({_id: this._id});
+  },
+	"click [data-action='edit']" : function(doc){
+		console.log("Editing " + this._id);
+		Session.set("editingCustomer", true);
   }
+
 });
