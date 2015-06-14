7ecd7204ba20e71f50d30c63dfe00bc1c4dfad4b
Dan Minshew <ofenixculpa@gmail.com>
2015-06-14 13:52:09 -0500

    general cleanup and update iron router

--- server/permissions.js
+++ server/permissions.js
@@ -21,18 +21,3 @@ Archives.allow({
     return true;
   }
 });
-
-// Images.allow({
-//   download: function () {
-//     return true;
-//   },
-//   insert: function () {
-//   	return true;
-//   },
-//   update: function () {
-//     return true;
-//   },
-//   remove: function () {
-//     return true;
-//   }
-// });
